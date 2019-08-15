8e85ede8889e8af915baa8610392f532171c31d4
Martijn van Iersel <mvaniersel@gmail.com>
2019-08-15 22:27:53 +0200

    Add missing file:

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es6": true,
+		"jasmine": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"sourceType": "module",
+		"ecmaVersion": 10
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-console": [
+			"off"
+		],
+		"eqeqeq": [ "error", "always" ],
+		/** don't allow underscores in names unless they're after this. 
+			Used to emulate private variables. */
+		"no-underscore-dangle": [
+			"error", 
+			{ "allowAfterThis": true }
+		]
+	}
+}
