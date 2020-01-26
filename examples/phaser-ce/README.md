78220a22cda5d6b9006bea3069c05c9c1b6ffa94
Martijn van Iersel <mvaniersel@gmail.com>
2020-01-26 13:47:07 +0100

    Phaser-ce example WIP

---
+++ examples/phaser-ce/README.md
@@ -0,0 +1,9 @@
+Project setup based on Phaser CE project templates: webpack
+https://github.com/photonstorm/phaser-ce/tree/master/resources/Project%20Templates
+
+Uses
+* Webpack 4
+* es-lint
+* Webpack dev server
+* AVA for testing
+* Phaser CE (Branch of Phaser 2)
--- examples/phaser-ce/src/main.js
+++ examples/phaser-ce/src/main.js
@@ -6,6 +6,9 @@
 import 'expose-loader?PIXI!phaser-ce/build/custom/pixi.js';
 import 'expose-loader?p2!phaser-ce/build/custom/p2.js';
 import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
+import SimpleTileMap from "./SimpleTileMap.js";
+import SimpleMap from "./SimpleMap.js";
+import { TilemapLayer } from 'phaser-ce';
 
 class Game extends Phaser.Game {
 	
@@ -26,17 +29,43 @@ class GameState {
 	
 	// called once per session
 	constructor() {
-		console.log("GameState.constructor");
+		console.log("GameState.constructor", arguments);
+	}
+
+	preload() {		
+		this.load.image("sprites1", "assets/sprites.png");
+		this.load.tilemap("tilemap1", "assets/level.json", null, Phaser.Tilemap.TILED_JSON);
+		// this.load.tilemap("tilemap1", "assets/level.json");
+		this.load.spritesheet("sprites2", "assets/sprites.png", 8, 8, 8);
 	}
 
 	// called everytime state is entered
 	create () {
+		this.game.stage.backgroundColor = "#787878";
+		this.game.stage.smoothed = false; // disable antialiasing
+
 		console.log("GameState.create");
 
 		const text = "- phaser -\n with a sprinkle of \n pixi dust.";
 		const style = { font: "65px Arial", fill: "#ff0044", align: "center" };
 	
-		/* const t = */ this.add.text(this.world.centerX - 300, 0, text, style);	
+		/* const t = */ this.add.text(this.world.centerX - 300, 0, text, style);
+
+		// const tilemap = new SimpleTileMap(8, 8, 20, 12, 1);
+		// for (let i = 0; i < 8; ++i) {
+		// 	tilemap.putTile(i, i, i);
+		// }
+		this.map = this.game.add.tilemap("tilemap1");
+		this.map.addTilesetImage("sprites", "sprites1");
+		this.l1 = this.map.createLayer("Tile Layer 1");
+		this.l1.scale.setTo(6.0);
+		this.l1.resizeWorld();
+
+		// const l1 = new TilemapLayer(this.game, map, 0);
+		// this.l1.addTilesetImage("sprites1");
+		// this.world.add(l1);
+
+		// create a tilemap
 	}
 }
 
