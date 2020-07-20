38b63ae0f04ced57f28458bcaed25b26a2fcd267
Martijn van Iersel <mvaniersel@gmail.com>
2020-07-20 13:31:49 +0200

    Create default exports with index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export {
+	astar,
+	breadthFirstSearch,
+	dijkstra
+} from "./pathFinding.js";
+
+export {
+	manhattanCrossProductHeuristic,
+	manhattanStraightHeuristic,
+	octagonalHeuristic
+} from "./astarHeuristics.js";
+
+export {
+	recursiveBackTracker
+} from "./maze.js";
