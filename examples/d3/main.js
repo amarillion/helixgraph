5ae2d24cef68fc84b2e379ea8922c440a8a7a45e
Martijn van Iersel <mvaniersel@gmail.com>
2020-10-18 13:21:36 +0200

    Add hexagonal option

--- examples/d3/main.js
+++ examples/d3/main.js
@@ -10,6 +10,99 @@ const BASE_COLOR = "white";
 
 // inspired by http://qiao.github.io/PathFinding.js/visual/
 
+const RECTANGULAR_4WAY = {
+	N: { key : "N", dx:  0, dy:  1, w: 1 },
+	E: { key : "E", dx:  1, dy:  0, w: 1 },
+	S: { key : "S", dx:  0, dy: -1, w: 1 },
+	W: { key : "W", dx: -1, dy:  0, w: 1 },
+};
+const RECTANGULAR_8WAY = {
+	...RECTANGULAR_4WAY,
+	NE: { key : "NE", dx:  1, dy:  1, w: 1.414 },
+	NW: { key : "NW", dx: -1, dy:  1, w: 1.414 },
+	SE: { key : "SE", dx:  1, dy: -1, w: 1.414 },
+	SW: { key : "SW", dx: -1, dy: -1, w: 1.414 },
+};
+const HEX_DIRS = {
+	0: { w: 1, dx: -1, dy: 0 },
+	1: { w: 1, dx: -1, dy: -1 },
+	2: { w: 1, dx: 0, dy: -1 },
+	3: { w: 1, dx: 1, dy: 0 },
+	4: { w: 1, dx: 1, dy: 1 },
+	5: { w: 1, dx: 0, dy: 1 },
+};
+
+let octagonalToggle = false;
+
+class RectangularCell {
+
+	constructor(x, y, grid) {
+		this.grid = grid;
+		this.x = x;
+		this.y = y;
+		this.blocked = false;
+		this.points = "0,0 0,30 30,30, 30,0";
+	}
+
+	neighborFunc() {
+		const DIRS_AVAILABLE = octagonalToggle ? RECTANGULAR_8WAY : RECTANGULAR_4WAY;
+		const result = [];
+		for (const key in DIRS_AVAILABLE) {
+			const { dx, dy } = DIRS_AVAILABLE[key];
+			const nx = this.x + dx;
+			const ny = this.y + dy;
+			if (!this.grid.inRange(nx, ny)) continue;
+			const cell = this.grid.get(nx, ny);
+			if (!cell.blocked)
+				result.push ([key, cell]);
+		}
+		return result;
+	}
+
+	get px() { return this.x * 30; }
+	get py() { return this.y * 30; }
+	get cx() { return this.px + 15; }
+	get cy() { return this.py + 15; }
+}
+
+class HexagonalCell {
+
+	constructor(x, y, grid) {
+		this.grid = grid;
+		this.x = x;
+		this.y = y;
+		this.blocked = false;
+		this.points = "0,10, 0,30 15,40, 30,30, 30,10, 15,0";
+	}
+
+	neighborFunc() {
+		const result = [];
+		for (const key in HEX_DIRS) {
+			let { dx, dy } = HEX_DIRS[key];
+			if (this.y % 2 === 0) {
+				if (dy === 1) dx--;
+			}
+			else {
+				if (dy === -1) dx++;
+			}
+			const nx = this.x + dx;
+			const ny = this.y + dy;
+			
+			if (!this.grid.inRange(nx, ny)) continue;
+			const cell = this.grid.get(nx, ny);
+			if (!cell.blocked)
+				result.push ([key, cell]);
+		}
+		return result;
+	}
+
+	get px() { return this.x * 30 + ((this.y % 2) * 15); }
+	get py() { return this.y * 30; }
+	get cx() { return this.px + 15; }
+	get cy() { return this.py + 20; }
+}
+
+
 class Main {
 	
 	constructor() {
@@ -18,14 +111,13 @@ class Main {
 		this.start = null;
 		this.goal = null;
 		this.validPath = false;
-		this.octagonalToggle = false;
 		this.mouseMode = null;
 
 		this.distanceSelect = document.getElementById("distance-select");
 		this.distanceSelect.options = [
 			{id: "manhattan", name:"Manhattan"},
 			{id: "euclidian", name:"Euclidian"}, 
-			{id: "octagonal", name:"Octagonal"}
+			{id: "octagonal", name:"8-Way"}
 		];
 
 		this.tiebreakerSelect = document.getElementById("tiebreaker-select");
@@ -37,7 +129,9 @@ class Main {
 
 		this.gridSelect = document.getElementById("grid-select");
 		this.gridSelect.options = [
-			{id:"rectangular", name:"Rectangular 4-way"}, { id:"octagonal", name: "Rectangular 8-way"}
+			{ id:"rectangular", name:"Rectangular 4-way"}, 
+			{ id:"octagonal", name: "Rectangular 8-way"},
+			{ id:"hexagonal", name: "Hexagonal"}
 		];
 
 		this.algorithmSelect = document.getElementById("algorithm-select");
@@ -53,8 +147,11 @@ class Main {
 			this.heuristic = this.heuristicFactory(this.start, this.goal);
 		};
 
-		this.gridSelect.callback = (newVal) => {
-			this.octagonalToggle = newVal === "octagonal";
+		this.gridSelect.callback = (newVal, oldVal) => {
+			octagonalToggle = newVal === "octagonal";
+			if (newVal === "hexagonal" || oldVal === "hexagonal") {
+				this.initGrid();
+			}
 			this.heuristic = this.heuristicFactory(this.start, this.goal);
 		};
 		
@@ -127,11 +224,7 @@ class Main {
 			.domain([0, maxDist, maxDist * 2])
 			.range(["orange", "white", "purple"]);
 
-		const rects = d3.select("svg")
-			.selectAll("rect")
-			.data(this.grid.eachNode());
-
-		rects.join(
+		this.cellSelection.join(
 			() => {},
 			update => update 
 				.attr("fill", d => d.blocked ? BLOCKED_COLOR : colorScale(this.heuristic(d)))
@@ -139,45 +232,18 @@ class Main {
 	}
 
 	findPath(source, dest, maxIterations) {
-		const dirs = {
-			N: { key : "N", dx:  0, dy:  1, w: 1 },
-			E: { key : "E", dx:  1, dy:  0, w: 1 },
-			S: { key : "S", dx:  0, dy: -1, w: 1 },
-			W: { key : "W", dx: -1, dy:  0, w: 1 },
-		};
-		const dirs2 = {
-			...dirs,
-			NE: { key : "NE", dx:  1, dy:  1, w: 1.414 },
-			NW: { key : "NW", dx: -1, dy:  1, w: 1.414 },
-			SE: { key : "SE", dx:  1, dy: -1, w: 1.414 },
-			SW: { key : "SW", dx: -1, dy: -1, w: 1.414 },
-		};
-		const dirsUsed = this.octagonalToggle ? dirs2 : dirs;
-
-		const grid = this.grid;
-		function *neighborFunc(tile) {
-			const { x, y } = tile;
-			for (const key in dirsUsed) {
-				const { dx, dy } = dirsUsed[key];
-				const nx = x + dx;
-				const ny = y + dy;
-				if (!grid.inRange(nx, ny)) continue;
-				const cell = grid.get(nx, ny);
-				if (!cell.blocked)
-					yield ([key, cell]);
-			}
-		}
-		const weightFunc = (edge) => dirsUsed[edge].w;
+		const weightFunc = octagonalToggle 
+			? (edge) => RECTANGULAR_8WAY[edge].w 
+			: () => 1;
 		const opts = { 
 			maxIterations,
 			getWeight: weightFunc,
 			getHeuristic: this.heuristic 
 		};
-		return this.algorithm(source, dest, neighborFunc, opts);
+		return this.algorithm(source, dest, node => node.neighborFunc(), opts);
 	}
 
 	drawPath(data, source, dest) {
-		
 		const maxDist = this.heuristic(this.start);
 		const colorScale = d3.scaleLinear()
 			.domain([0, maxDist * 0.6, maxDist * 1.2])
@@ -194,8 +260,8 @@ class Main {
 		if (this.validPath) {
 			//This is the accessor function we talked about above
 			var lineFunction = d3.line()
-				.x(d => d.cx())
-				.y(d => d.cy());
+				.x(d => d.cx)
+				.y(d => d.cy);
 
 			// clear old path
 			d3.selectAll("#route").remove();
@@ -232,8 +298,8 @@ class Main {
 	updateViz() {
 		if (!this.grid) return;
 
-		const rects = d3.select("svg")
-			.selectAll("rect")
+		this.cellSelection = d3.select("svg")
+			.selectAll("polygon")
 			.data(this.grid.eachNode());
 
 		const mapFillColor = (d) => {
@@ -241,16 +307,19 @@ class Main {
 			return BASE_COLOR;
 		};
 
-		rects.join(
+		this.cellSelection.join(
 			enter => enter
-				.append("rect")
+				.append("polygon")
+				// .append("rect")
 				.each(
 					// store reference to SVG element.
 					// if we use old fashioned function notation, 'this' is bound to svg element.
 					function(d) { d.elt = this; } 
 				)
-				.attr("x", d => d.px())
-				.attr("y", d => d.py())
+				.attr("points", d => d.points)
+				.attr("transform", d => {
+					return `translate(${d.px}, ${d.py})`;	
+				})
 				.attr("fill", mapFillColor)
 				.attr("width", 30)
 				.attr("height", 30)
@@ -279,8 +348,8 @@ class Main {
 			.data([this.start, this.goal])
 			.join("circle")
 			.attr("r", 15)
-			.attr("cx", d => d.cx())
-			.attr("cy", d => d.cy())
+			.attr("cx", d => d.cx)
+			.attr("cy", d => d.cy)
 			.attr("fill", (d, /* i */) => {
 				if (d === this.start) return "lightgreen";
 				else return "crimson";
@@ -289,31 +358,33 @@ class Main {
 		// this.vizHeuristic();
 	}
 
-	init () {
+	initGrid() {
 		const w = document.body.clientWidth;
 		const h = document.body.clientHeight;
 
+		const isHexagonal = this.gridSelect.value === "hexagonal";
 		this.grid = new BaseGrid(
 			Math.ceil(w / 30), Math.ceil(h / 30), 
-			(x, y) => ({ 
-				x, y, blocked: false,
-				px: () => x * 30,
-				py: () => y * 30,
-				cx: () => x * 30 + 15,
-				cy: () => y * 30 + 15
-			}) 
+			(x, y, grid) => 
+				isHexagonal 
+					? new HexagonalCell(x, y, grid) 
+					: new RectangularCell(x, y, grid)
 		);
 
-		// const mid = Math.floor(this.grid.height / 2);
 		this.start = this.grid.get(5, this.grid.height - 5);
 		this.goal = this.grid.get(this.grid.width - 5, 5);
-		
+
+		d3.select("svg").selectAll("polygon").remove();
+		d3.select("svg").selectAll("circle").remove();
+
 		this.heuristic = this.heuristicFactory(this.start, this.goal);
-
-		this.initViz();
-
 		this.resetPath();
-		
+	}
+
+	init() {
+		this.initViz();
+		this.initGrid();
+
 		setInterval(
 			() => this.update(), 20);
 	}
@@ -350,6 +421,7 @@ class Select extends HTMLElement {
 		this.binding = null;
 		this.render();
 		this._callback = () => {};
+		this.oldValue = null;
 	}
 
 	set label(val) {
@@ -365,7 +437,9 @@ class Select extends HTMLElement {
 	set callback(val) {
 		this._callback = val;
 		// immediately trigger with current value
-		this._callback(this.shadowRoot.querySelector("select").value);
+		const newValue = this.shadowRoot.querySelector("select").value;
+		this._callback(newValue, this.oldValue);
+		this.oldValue = newValue;
 	}
 
 	get value() {
@@ -373,7 +447,8 @@ class Select extends HTMLElement {
 	}
 
 	onChange(event) {
-		this._callback(event.target.value);
+		this._callback(event.target.value, this.oldValue);
+		this.oldValue = event.target.value;
 	}
 
 	render() {
