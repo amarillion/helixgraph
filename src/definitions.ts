855bbffe321da7975828e59c3ba02c69d1746548
Martijn van Iersel <mvaniersel@gmail.com>
2021-10-17 09:34:20 +0200

    Tweak definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -10,7 +10,7 @@ export type AdjacencyFunc<N, E> = (from: N) => Iterable<[E, N]>;
 export type LinkFunc<N, E> = (from: N, edge: E, to: N) => boolean
 
 export type PathFindFunc<N, E> = 
-	(src: N, dest: N | N[], getAdjacent: AdjacencyFunc<N, E>, options: Record<string, unknown>) => Map<N, Step<N, E>>;
+	(src: N, dest: N | N[], getAdjacent: AdjacencyFunc<N, E>, options: { [key: string] : unknown }) => Map<N, Step<N, E>>;
 
 /** 
  * Weight for a given edge.
