4b881d095aac1d61a2e3d7c0fc49fe8eb47e4bd3
Martijn van Iersel <mvaniersel@gmail.com>
2022-04-25 21:36:42 +0200

    Merge 0.7, add p5-kruskal example

---
+++ examples/p5-kruskal/animated-maze-component.js
@@ -0,0 +1,9 @@
+import { P5Wrapper } from "./p5-wrapper.js";
+import { createRobinMaze } from "./animated-maze.js";
+
+export class AnimatedMazeComponent extends P5Wrapper {
+
+	constructor() {
+		super(createRobinMaze);
+	}
+}
