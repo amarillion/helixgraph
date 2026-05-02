5afd3e00810e8465ea75502bfc77e36e636fffbd
Martijn van Iersel <mvaniersel@gmail.com>
2026-05-02 10:01:39 +0200

    Maze example: polar and square grid options

--- examples/maze/main.js
+++ examples/maze/main.js
@@ -1,86 +1,13 @@
 import { recursiveBackTracker } from "../../lib/maze/recursiveBacktracker.js";
 import { pickOne } from "../../lib/random.js";
-import { BaseGrid, NORTH, SOUTH, EAST, WEST } from "../../lib/BaseGrid.js";
 import { prim, PRIM_LAST_ADDED_RANDOM_EDGES, PRIM_RANDOM } from "../../lib/maze/prim.js";
 import { kruskal } from "../../lib/maze/kruskal.js";
 import { aldousBroder } from "../../lib/maze/aldousBroder.js";
-import { Collapsible, Select } from "../util/components.js";
+import { Checkbox, Collapsible, Select } from "../util/components.js";
 import { assert } from "../../lib/assert.js";
-
-// for being able to find the opposite direction
-const reverse = {
-	[NORTH]: SOUTH,
-	[SOUTH]: NORTH,
-	[EAST]: WEST,
-	[WEST]: EAST
-};
-
-const CELL_SIZE = 20;
-
-const POINTS = [
-	{ x: 0, y: 0 },
-	{ x: CELL_SIZE, y: 0 },
-	{ x: CELL_SIZE, y: CELL_SIZE },
-	{ x: 0, y: CELL_SIZE }
-];
-
-const SEGMENTS = {
-	[NORTH]: POINTS.slice(0, 2),
-	[EAST]: POINTS.slice(1, 3),
-	[SOUTH]: POINTS.slice(2, 4),
-	[WEST]: [ POINTS[3], POINTS[0] ]
-};
-
-const margin = 10;
-
-// cell implementation that keeps track of links to neighboring cells
-class Cell {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-		this.links = {};
-	}
-
-	/**
-	 * @param {*} other cell to link to
-	 * @param {*} dir one of NORTH, EAST, SOUTH, WEST
-	 * @param {*} reverse optional - supply a reverse direction if you want to make
-	 *   the link bidirectional
-	 */
-	link(other, dir, reverse) {
-		if (dir in this.links) {
-			console.log("WARNING: creating link that already exists: ", { dir, reverse });
-		}
-		this.links[dir] = other;
-		if (reverse) {
-			// call recursively, but without reversing again
-			other.link(this, reverse);
-		}
-	}
-
-	linked(dir) {
-		return dir in this.links;
-	}
-
-	get px() { return this.x * CELL_SIZE; }
-	get py() { return this.y * CELL_SIZE; }
-	
-	render(ctx) {
-		ctx.lineWidth = 1.0;
-		ctx.strokeStyle = "black";
-
-		// ctx.lineCap = "round";
-		for (const dir of [ NORTH, EAST, SOUTH, WEST ]) {
-			if (this.linked(dir)) continue;
-
-			const segment = SEGMENTS[dir];
-			ctx.beginPath();
-			ctx.moveTo(margin + this.px + segment[0].x, margin + this.py + segment[0].y);
-			ctx.lineTo(margin + this.px + segment[1].x, margin + this.py + segment[1].y);
-			ctx.stroke();
-		}
-	}
-}
+import { EAST, NORTH } from "../../lib/BaseGrid.js";
+import { createSquareGrid } from "./square.js";
+import { createPolarGrid } from "./polar.js";
 
 // antoher alternative maze generation algorithm
 // THIS works only with a rectangular grid...
@@ -98,9 +25,10 @@ export function binaryTree(grid, linkCells, prng = Math.random) {
 }
 
 customElements.define("hxg-collapsible", Collapsible);
+customElements.define("hxg-checkbox", Checkbox);
 customElements.define("hxg-select", Select);
 
-const linkCells = (src, dir, dest) => { src.link(dest, dir, reverse[dir]); };
+const linkCells = (src, dir, dest) => { src.link(dest, dir); };
 
 class Main {
 	refreshCanvas() {
@@ -114,20 +42,27 @@ class Main {
 	}
 
 	refreshMaze() {
+		this.refreshGrid();
+
+		this.algorithm(this.grid, linkCells);
+		const ctx = this.canvas.getContext("2d");
+		ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		for (const node of this.grid.eachNode()) {
+			node.render(ctx);
+		}
+	}
+
+	refreshGrid() {
 		const canvasWidth = (document.body.clientWidth);
 		const canvasHeight = (document.body.clientHeight);
 
-		const cellFactory = (x, y) => new Cell(x, y);
-		const grid = new BaseGrid(
-			Math.floor((canvasWidth - margin * 2) / CELL_SIZE),
-			Math.floor((canvasHeight - margin * 2) / CELL_SIZE),
-			cellFactory
-		);
-		this.algorithm(grid);
-		const ctx = this.canvas.getContext("2d");
-		ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		for (const node of grid.eachNode()) {
-			node.render(ctx);
+		switch(this.gridSelect.value) {
+			case 'polar':
+				this.grid = createPolarGrid(canvasWidth, canvasHeight);
+				break;
+			default:
+				this.grid = createSquareGrid(canvasWidth, canvasHeight);
+				break;
 		}
 	}
 
@@ -178,6 +113,8 @@ class Main {
 		this.canvas = document.getElementById("myCanvas");
 	
 		this.algorithmSelect = document.getElementById("algorithm-select");
+		this.gridSelect = document.getElementById("grid-select");
+
 		this.algorithmSelect.options = [
 			{ id: "recursivebt", name: "Recursive Backtracker" },
 			{ id: "kruskal", name: "Kruskal's algorithm" },
@@ -191,11 +128,28 @@ class Main {
 			this.refreshAlgorithm();
 		};
 	
+		this.gridSelect.options = [
+			{ id: "square", name: "Square" },
+			{ id: "polar", name: "Polar" },
+			/*
+			{ id: "hexagonal", name: "Hexagonal" },
+			{ id: "diamonds", name: "Diamonds" },
+			{ id: "cairo", name: "Cairo" },
+			{ id: "triangular", name: "Triangular" },
+			{ id: "voronoi", name: "Voronoi" },
+			 */
+		];
+		
+		this.gridSelect.callback = () => {
+			this.refreshMaze();
+		};
+		
 		window.onresize = () => {
 			this.refreshCanvas();
 		};
 
 		this.refreshCanvas();
+
 	}
 }
 
